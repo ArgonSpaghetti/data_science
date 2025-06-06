81e17d1c397ccedd928d167a12476bc4d57fe2d9
ArgonSpaghetti <alessandromazzucchi64@gmail.com>
2025-06-06 20:29:12 +0200

    Update gender.py
    modified variable name

--- gender.py
+++ gender.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 
 
-shoe_size = [[181,80,44],[177,70,43],[160,60,38],
+shoe_size_height_weight = [[181,80,44],[177,70,43],[160,60,38],
      [154,54,37],[166,65,40],[190,90,47],
      [175,64,38],[177,70,40],[159,55,37],
     [171,75,42],[181,85,43]]
@@ -13,8 +13,7 @@ gender = ["male","female","female","female",
 
 clf = tree.DecisionTreeClassifier()
 
-clf = clf.fit(shoe_size, gender)
+clf = clf.fit(shoe_size_height_weight, gender)
 
-pred = clf.predict([[190,70,43]])
-
+pred = clf.predict([[170, 70, 40]])
 print(pred)
