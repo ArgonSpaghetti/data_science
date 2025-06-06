18b2ccadba7e4cc69f0b8420c82e64ce05922b75
argon <alessandromazzucchi64@gmail.com>
2025-06-06 17:17:50 +0200

    scikit-learn prediction tree

---
+++ gender.py
@@ -0,0 +1,20 @@
+from sklearn import tree
+
+
+shoe_size = [[181,80,44],[177,70,43],[160,60,38],
+     [154,54,37],[166,65,40],[190,90,47],
+     [175,64,38],[177,70,40],[159,55,37],
+    [171,75,42],[181,85,43]]
+
+gender = ["male","female","female","female",
+     "male", "male", "male", "female","male", 
+     "female", "male"]
+
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(shoe_size, gender)
+
+pred = clf.predict([[190,70,43]])
+
+print(pred)
